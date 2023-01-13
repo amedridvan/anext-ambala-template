04d2b9c5e1c145fa817929971719a1d410765d78
osmanbabaapp <osmanbabaapp@gmail.com>
2023-01-13 17:34:41 +0300

    update

--- layouts/sections/section.tsx
+++ layouts/sections/section.tsx
@@ -3,19 +3,13 @@ import Image from "next/image";
 import img1 from  '../../public/image.jpg'
 import { useState } from 'react';
 import img2 from '../../public/i5.jpg'
+import Document from '@/pages/_document';
 function Sec(){
-  const [clickedButton, setClickedButton] = useState('');
-  
+const [clickedButton, setClickedButton] = useState('');
 const buttonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     const button: HTMLButtonElement = event.currentTarget;
-    let a=``
-    switch(button.name){
-      case "b2":
-        a=`ahmed`
-        break;
-
-    }
+    let a=`231`
     setClickedButton(a)
   };
     return(
@@ -41,24 +35,11 @@ const buttonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
       <button  name="b6" className="ml-[20px] border border-black border-solid pb-[5px] px-[5px]  my-[5px]   rounded cursor-text" onClick={buttonHandler}>AGRICULTURAL</button>
       <button  name="b7" className="ml-[20px] border border-black border-solid pb-[5px] px-[5px]  my-[5px]   rounded cursor-text" onClick={buttonHandler}>BIODEGRADABLE</button>
       </div>
-      <div className="bg-zinc-700 h-[200px] relative mt-[30px] text-center p-[10px]">
-      {clickedButton !== ""
-          ? `You have clicked "${clickedButton}"` 
-          : "No button clicked yet"}
+      <div  id="a2" className="bg-zinc-700 h-[200px] relative mt-[30px] text-center p-[10px]">
+      {clickedButton}
       </div>
       </div>
-      <div className="w-[400px] flex justify-center">
-        <Image  className=" absolute z-0 h-[120px] w-[420px]  "
-        src={img2}
-        alt="cover foto"
-        objectPosition="center"
-        objectFit="cover"/>
-       <div className="relative text-black flex flex-col text-center ">
-         <span>food</span>
-         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
-         <span>readmoer</span>
-       </div>
-       </div>
+      
         </>
 )
 }
